31c4d1d74b9a1d9ff851993ce195643086e1f1ee
blackcat <623970143@qq.com>
2020-06-01 12:30:32 +0800

    remove dist

--- dist/components/bussniess/Controller/index.d.ts
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-interface Props {
-    keyboard: boolean;
-    onClose(): void;
-}
-declare const _default: React.NamedExoticComponent<Props>;
-export default _default;
--- dist/components/common/Tooltip/index.d.ts
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-interface Props {
-    content: React.ReactNode;
-    children: React.ReactNode;
-}
-declare const _default: React.NamedExoticComponent<Props>;
-export default _default;
